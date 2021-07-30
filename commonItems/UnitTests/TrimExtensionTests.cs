520aeb529f13fbd7d0fb398c62aea5035c3c55fa
IhateTrains <IhateTrains@users.noreply.github.com>
2021-07-30 23:18:54 +0200

    Split up CommonFunctionsTests

---
+++ commonItems/UnitTests/TrimExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace commonItems.UnitTests {
+    public class TrimExtensionTests {
+        [Fact]
+        public void TrimExtensionTrimsDot() {
+            var input = @"file.extension";
+            Assert.Equal(@"file", CommonFunctions.TrimExtenstion(input));
+        }
+        [Fact]
+        public void TrimExtensionTrimsLastDot() {
+            var input = @"file.name.with.extension";
+            Assert.Equal(@"file.name.with", CommonFunctions.TrimExtenstion(input));
+        }
+        [Fact]
+        public void TrimExtensionDoesNotAffectDirectories() {
+            var input = @"/path/with.extension/filename";
+            Assert.Equal(@"/path/with.extension/filename", CommonFunctions.TrimExtenstion(input));
+        }
+    }
+}
