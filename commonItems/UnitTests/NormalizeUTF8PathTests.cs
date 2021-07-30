489fa71011f2ee4489ad7058d0825b2a8f7027e3
IhateTrains <IhateTrains@users.noreply.github.com>
2021-07-31 01:31:24 +0200

    Create NormalizeUTF8PathTests.cs

---
+++ commonItems/UnitTests/NormalizeUTF8PathTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace commonItems.UnitTests {
+    public class NormalizeUTF8PathTests {
+        [Fact] public void NonAllowedCharactersAreReplaced() {
+            var str = @"/\:*?""<>|";
+            Assert.Equal("_________", CommonFunctions.NormalizeUTF8Path(str));
+        }
+        [Fact]
+        public void AllowedCharactersAreNotReplaced() {
+            var str = "string123";
+            Assert.Equal("string123", CommonFunctions.NormalizeUTF8Path(str));
+        }
+        [Fact]
+        public void TabsAreSkipped() {
+            var str = "\tstring123\t";
+            Assert.Equal("string123", CommonFunctions.NormalizeUTF8Path(str));
+        }
+    }
+}
