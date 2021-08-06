be60dea5af29bb6333a467257d42dbdb07e6380a
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-06 11:09:37 +0200

    ModParserTests

--- commonItems/UnitTests/DateTests.cs
+++ commonItems/UnitTests/DateTests.cs
@@ -3,6 +3,8 @@ using System.IO;
 using Xunit;
 
 namespace commonItems.UnitTests {
+    [Collection("Sequential")]
+    [CollectionDefinition("Sequential", DisableParallelization = true)]
     public class DateTests {
         readonly int decimalPlaces = 4;
 
