9c37f5849b2b6a41261fde4fecc5e285a94e29ef
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-06 10:50:14 +0200

    ModParser

---
+++ commonItems/ModParser.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace commonItems.ModLoader {
+    public class ModParser : Parser {
+        public string Name { get; private set; } = "";
+        public string Path { get; set; } = "";
+
+        private bool compressed = false;
+        public SortedSet<string> Dependencies { get; private set; } = new();
+
+        public ModParser() { }
+        public void ParseMod(BufferedReader reader) {
+            RegisterKeys();
+            ParseStream(reader);
+            ClearRegisteredRules();
+            CheckIfCompressed();
+        }
+        public void ParseMod(string fileName) {
+            RegisterKeys();
+            ParseFile(fileName);
+            ClearRegisteredRules();
+            CheckIfCompressed();
+        }
+        private void CheckIfCompressed() {
+            if (!string.IsNullOrEmpty(Path)) {
+                var ending = CommonFunctions.GetExtension(Path);
+                compressed = ending == "zip" || ending == "bin";
+            }
+        }
+        public bool IsValid() {
+            return !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Path);
+        }
+        public bool IsCompressed() {
+            return compressed;
+        }
+
+        private void RegisterKeys() {
+            RegisterKeyword("name", (sr) => {
+                Name = new SingleString(sr).String;
+            });
+            RegisterRegex("path|archive", (sr) => {
+                Path = new SingleString(sr).String;
+            });
+            RegisterKeyword("dependencies", (sr) => {
+                var newDependencies = new StringList(sr).Strings;
+                Dependencies.UnionWith(newDependencies);
+            });
+            RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreItem);
+        }
+    }
+}
