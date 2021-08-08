0a9e38322e5b01217d3662f9692323c07b6bb37f
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-08 14:12:35 +0200

    Update Mod.cs

--- commonItems/Mod.cs
+++ commonItems/Mod.cs
@@ -4,27 +4,29 @@ using System.Collections.Generic;
 using Name = System.String;
 using Path = System.String;
 
-public class Mod {
-    public Name name = "";
-    public Path path = "";
-    public SortedSet<Name> dependencies = new();
-    public Mod() { }
-    public Mod(Name name, Path path) {
-        this.name = name;
-        this.path = path;
-    }
-    public Mod(Name name, Path path, SortedSet<Name> dependencies) {
-        this.name = name;
-        this.path = path;
-        this.dependencies = dependencies;
-    }
+namespace commonItems {
+    public class Mod {
+        public Name name = "";
+        public Path path = "";
+        public SortedSet<Name> dependencies = new();
+        public Mod() { }
+        public Mod(Name name, Path path) {
+            this.name = name;
+            this.path = path;
+        }
+        public Mod(Name name, Path path, SortedSet<Name> dependencies) {
+            this.name = name;
+            this.path = path;
+            this.dependencies = dependencies;
+        }
 
-    public override bool Equals(object? obj) {
-        return obj is Mod mod &&
-               name == mod.name &&
-               System.IO.Path.GetFullPath(path) == System.IO.Path.GetFullPath(mod.path);
-    }
-    public override int GetHashCode() {
-        return HashCode.Combine(name, path);
+        public override bool Equals(object? obj) {
+            return obj is Mod mod &&
+                   name == mod.name &&
+                   System.IO.Path.GetFullPath(path) == System.IO.Path.GetFullPath(mod.path);
+        }
+        public override int GetHashCode() {
+            return HashCode.Combine(name, path);
+        }
     }
 }
