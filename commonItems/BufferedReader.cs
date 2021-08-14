0542ff6378283ac334e861795e3f84ff4beeb7a9
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-14 14:52:46 +0200

    BufferedReader.StreamReader

--- commonItems/BufferedReader.cs
+++ commonItems/BufferedReader.cs
@@ -12,14 +12,14 @@ namespace commonItems {
     /// but later updated
     /// </summary>
     public class BufferedReader {
-        private readonly StreamReader streamReader;
+        public StreamReader StreamReader { get; }
 
-        public BufferedReader(StreamReader reader) { streamReader = reader; }
-        public BufferedReader(Stream stream) { streamReader = new StreamReader(stream); }
+        public BufferedReader(StreamReader reader) { StreamReader = reader; }
+        public BufferedReader(Stream stream) { StreamReader = new StreamReader(stream); }
 
         public BufferedReader(string input) {
             var stream = new MemoryStream(Encoding.UTF8.GetBytes(input));
-            streamReader = new StreamReader(stream);
+            StreamReader = new StreamReader(stream);
         }
 
         private readonly Stack<int> characterStack = new();
@@ -29,16 +29,14 @@ namespace commonItems {
             if (characterStack.TryPop(out int character)) {
                 ch = character;
             } else {
-                ch = streamReader.Read();  // could be -1 
+                ch = StreamReader.Read();  // could be -1 
             }
             return ch;
         }
 
-        public string Read(uint numberOfChars)
-        {
+        public string Read(uint numberOfChars) {
             var sb = new StringBuilder();
-            for (uint i = 0; i < numberOfChars; ++i)
-            {
+            for (uint i = 0; i < numberOfChars; ++i) {
                 var ch = Read();
                 if (ch == -1)
                     break;
@@ -48,9 +46,8 @@ namespace commonItems {
             return sb.ToString();
         }
 
-        public string? ReadLine()
-        {
-            return streamReader.ReadLine();
+        public string? ReadLine() {
+            return StreamReader.ReadLine();
         }
 
         public string ReadToEnd() {
@@ -58,22 +55,22 @@ namespace commonItems {
             while (characterStack.TryPop(out int character)) {
                 sb.Append((char)character);
             }
-            sb.Append(streamReader.ReadToEnd());
+            sb.Append(StreamReader.ReadToEnd());
             return sb.ToString();
         }
 
         public int Peek() {
-            return characterStack.TryPeek(out int character) ? character : streamReader.Peek();
+            return characterStack.TryPeek(out int character) ? character : StreamReader.Peek();
         }
 
         public bool EndOfStream {
             get {
                 if (characterStack.Count == 0) {
-                    return streamReader.EndOfStream;
+                    return StreamReader.EndOfStream;
                 }
                 return false;
             }
-        } 
+        }
 
         public void Skip(uint numberOfBytes) // not present in StreamReader
         {
@@ -82,8 +79,7 @@ namespace commonItems {
             }
         }
 
-        public void PushBack(char ch)
-        {
+        public void PushBack(char ch) {
             characterStack.Push(ch);
         }
     }
