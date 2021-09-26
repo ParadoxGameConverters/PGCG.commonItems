5a9cae3a3a6ed21f00fc485bc339b633d875e031
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-09-26 21:24:42 +0200

    ParadoxBool (#39)
    * Update ParserHelpers.cs
    
    * Update SharpZipLib
    
    * ParadoxBool
    
    * remove redundant assignment

---
+++ commonItems.UnitTests/ParadoxBoolTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+
+namespace commonItems.UnitTests {
+	public class ParadoxBoolTests {
+		[Fact]
+		public void ParadoxBoolDefaultToTrue() {
+			Assert.True(new ParadoxBool());
+		}
+		[Fact]
+		public void ParadoxBoolCanBeConstructedFromString() {
+			Assert.True(new ParadoxBool("yes"));
+			Assert.False(new ParadoxBool("no"));
+		}
+		[Fact]
+		public void ExceptionIsThrownOnWrongValueString() {
+			const string valStr = "perhaps";
+			Assert.Throws<FormatException>(() => new ParadoxBool(valStr));
+		}
+		[Fact]
+		public void ParadoxBoolCanBeConstructedFromBufferedReader() {
+			var reader1 = new BufferedReader("= yes");
+			var reader2 = new BufferedReader("= no");
+			Assert.True(new ParadoxBool(reader1));
+			Assert.False(new ParadoxBool(reader2));
+		}
+		[Fact]
+		public void CorrectTextRepresentationIsReturned() {
+			var bool1 = new ParadoxBool("yes");
+			var bool2 = new ParadoxBool("no");
+			Assert.Equal("yes", bool1.YesOrNo);
+			Assert.Equal("no", bool2.YesOrNo);
+		}
+		[Fact]
+		public void ValueCanBeChanged() {
+			var pdxBool = new ParadoxBool("no");
+			Assert.False(pdxBool);
+			pdxBool.Value = true;
+			Assert.True(pdxBool);
+		}
+	}
+}
---
+++ commonItems/ParadoxBool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace commonItems {
+	public class ParadoxBool {
+		public bool Value { get; set; } = true;
+		public string YesOrNo {
+			get {
+				if (Value) {
+					return "yes";
+				}
+				return "no";
+			}
+		}
+		public ParadoxBool() { }
+		public ParadoxBool(string valueString) {
+			if (valueString == "yes") {
+				Value = true;
+			} else if (valueString == "no") {
+				Value = false;
+			} else {
+				throw new FormatException("Text representation of ParadoxBool should be \"yes\" or \"no\"!");
+			}
+		}
+		public ParadoxBool(BufferedReader reader) : this(ParserHelpers.GetString(reader)) { }
+		public static implicit operator bool(ParadoxBool m) {
+			return m?.Value == true;
+		}
+	}
+}
