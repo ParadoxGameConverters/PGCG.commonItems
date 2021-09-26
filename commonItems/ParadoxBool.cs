53eb23a5c8a037c6d5e2304750659c4f7365fd6b
IhateTrains <IhateTrains@users.noreply.github.com>
2021-09-26 21:09:17 +0200

    ParadoxBool

---
+++ commonItems/ParadoxBool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace commonItems {
+	public class ParadoxBool {
+		public bool Value { get; set; } = true;
+		public string YesOrNo {
+			get {
+				if (Value) {
+					return "yes";
+				}
+				return "no";
+			}
+		}
+		public ParadoxBool() {
+			Value = true;
+		}
+		public ParadoxBool(string valueString) {
+			if (valueString == "yes") {
+				Value = true;
+			} else if (valueString == "no") {
+				Value = false;
+			} else {
+				throw new FormatException("Text representation of ParadoxBool should be \"yes\" or \"no\"!");
+			}
+		}
+		public ParadoxBool(BufferedReader reader) : this(ParserHelpers.GetString(reader)) { }
+		public static implicit operator bool(ParadoxBool m) {
+			return m?.Value == true;
+		}
+	}
+}
