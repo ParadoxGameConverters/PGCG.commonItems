c2702a9dd12a02bdcdc0eff5418a62893a6df063
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-10-08 18:07:49 +0200

    Notice log level (#45)
    * Notice log level
    
    * Format versions of log methods

--- commonItems.UnitTests/LoggerTests.cs
+++ commonItems.UnitTests/LoggerTests.cs
@@ -3,47 +3,97 @@ using System.IO;
 using Xunit;
 
 namespace commonItems.UnitTests {
-    [Collection("Sequential")]
-    [CollectionDefinition("Sequential", DisableParallelization = true)]
-    public class LoggerTests {
-        [Fact]
-        public void ErrorMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Error("Error message");
-            Assert.Equal("[ERROR] Error message", output.ToString().TrimEnd());
-        }
+	[Collection("Sequential")]
+	[CollectionDefinition("Sequential", DisableParallelization = true)]
+	public class LoggerTests {
+		[Fact]
+		public void ErrorMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Error("Error message");
+			Assert.Equal("[ERROR] Error message", output.ToString().TrimEnd());
+		}
+		[Fact]
+		public void ErrorMessagesFormatLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.ErrorFormat("Error {0}", "message");
+			Assert.Equal("[ERROR] Error message", output.ToString().TrimEnd());
+		}
 
-        [Fact]
-        public void WarningMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Warn("Warning message");
-            Assert.Equal("[WARN] Warning message", output.ToString().TrimEnd());
-        }
+		[Fact]
+		public void WarningMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Warn("Warning message");
+			Assert.Equal("[WARN] Warning message", output.ToString().TrimEnd());
+		}
+		[Fact]
+		public void WarningMessagesFormatLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.WarnFormat("Warning {0}", "message");
+			Assert.Equal("[WARN] Warning message", output.ToString().TrimEnd());
+		}
 
-        [Fact]
-        public void InfoMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Info("Info message");
-            Assert.Equal("[INFO] Info message", output.ToString().TrimEnd());
-        }
+		[Fact]
+		public void InfoMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Info("Info message");
+			Assert.Equal("[INFO] Info message", output.ToString().TrimEnd());
+		}
+		[Fact]
+		public void InfoMessagesFormatLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.InfoFormat("Info {0}", "message");
+			Assert.Equal("[INFO] Info message", output.ToString().TrimEnd());
+		}
 
-        [Fact]
-        public void DebugMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Debug("Debug message");
-            Assert.Equal("[DEBUG] Debug message", output.ToString().TrimEnd());
-        }
+		[Fact]
+		public void NoticeMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Notice("Notice message");
+			Assert.Equal("[NOTICE] Notice message", output.ToString().TrimEnd());
+		}
+		[Fact]
+		public void NoticeMessagesFormatLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.NoticeFormat("Notice {0}", "message");
+			Assert.Equal("[NOTICE] Notice message", output.ToString().TrimEnd());
+		}
 
-        [Fact]
-        public void ProgressMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Progress("Progress message");
-            Assert.Equal("[PROGRESS] Progress message", output.ToString().TrimEnd());
-        }
-    }
+		[Fact]
+		public void DebugMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Debug("Debug message");
+			Assert.Equal("[DEBUG] Debug message", output.ToString().TrimEnd());
+		}
+		[Fact]
+		public void DebugMessagesFormatLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.DebugFormat("Debug {0}", "message");
+			Assert.Equal("[DEBUG] Debug message", output.ToString().TrimEnd());
+		}
+
+		[Fact]
+		public void ProgressMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Progress("Progress message");
+			Assert.Equal("[PROGRESS] Progress message", output.ToString().TrimEnd());
+		}
+		[Fact]
+		public void ProgressMessagesFormatLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.ProgressFormat("Progress {0}", "message");
+			Assert.Equal("[PROGRESS] Progress message", output.ToString().TrimEnd());
+		}
+	}
 }
--- commonItems/Logger.cs
+++ commonItems/Logger.cs
@@ -1,32 +1,53 @@
-﻿using System.IO;
-using log4net;
+﻿using log4net;
 using log4net.Config;
+using System.IO;
 
 namespace commonItems {
-    public static class Logger {
-        private static readonly ILog log = LogManager.GetLogger("mainLogger");
-        static Logger() {
-            // add custom "PROGRESS" level
-            LogManager.GetRepository().LevelMap.Add(LogExtensions.progressLevel);
+	public static class Logger {
+		private static readonly ILog log = LogManager.GetLogger("mainLogger");
+		static Logger() {
+			// add custom "PROGRESS" level
+			LogManager.GetRepository().LevelMap.Add(LogExtensions.progressLevel);
 
-            // configure log4net
-            var logConfiguration = new FileInfo("log4net.config");
-            XmlConfigurator.Configure(logConfiguration);
-        }
-        public static void Error(string message) {
-            log.Error(message);
-        }
-        public static void Warn(string message) {
-            log.Warn(message);
-        }
-        public static void Info(string message) {
-            log.Info(message);
-        }
-        public static void Debug(string message) {
-            log.Debug(message);
-        }
-        public static void Progress(string message) {
-            log.Progress(message);
-        }
-    }
+			// configure log4net
+			var logConfiguration = new FileInfo("log4net.config");
+			XmlConfigurator.Configure(logConfiguration);
+		}
+		public static void Error(string message) {
+			log.Error(message);
+		}
+		public static void ErrorFormat(string message, params object[] args) {
+			log.ErrorFormat(message, args);
+		}
+		public static void Warn(string message) {
+			log.Warn(message);
+		}
+		public static void WarnFormat(string message, params object[] args) {
+			log.WarnFormat(message, args);
+		}
+		public static void Info(string message) {
+			log.Info(message);
+		}
+		public static void InfoFormat(string message, params object[] args) {
+			log.InfoFormat(message, args);
+		}
+		public static void Notice(string message) {
+			log.Notice(message);
+		}
+		public static void NoticeFormat(string message, params object[] args) {
+			log.NoticeFormat(message, args);
+		}
+		public static void Debug(string message) {
+			log.Debug(message);
+		}
+		public static void DebugFormat(string message, params object[] args) {
+			log.DebugFormat(message, args);
+		}
+		public static void Progress(string message) {
+			log.Progress(message);
+		}
+		public static void ProgressFormat(string message, params object[] args) {
+			log.ProgressFormat(message, args);
+		}
+	}
 }
