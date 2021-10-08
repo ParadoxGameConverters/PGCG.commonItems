9ee3ac619e9a7f465ae41fcabe427b68d6b8bf0b
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-10-08 17:48:12 +0200

    Notice log level

--- commonItems.UnitTests/LoggerTests.cs
+++ commonItems.UnitTests/LoggerTests.cs
@@ -3,47 +3,55 @@ using System.IO;
 using Xunit;
 
 namespace commonItems.UnitTests {
-    [Collection("Sequential")]
-    [CollectionDefinition("Sequential", DisableParallelization = true)]
-    public class LoggerTests {
-        [Fact]
-        public void ErrorMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Error("Error message");
-            Assert.Equal("[ERROR] Error message", output.ToString().TrimEnd());
-        }
+	[Collection("Sequential")]
+	[CollectionDefinition("Sequential", DisableParallelization = true)]
+	public class LoggerTests {
+		[Fact]
+		public void ErrorMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Error("Error message");
+			Assert.Equal("[ERROR] Error message", output.ToString().TrimEnd());
+		}
 
-        [Fact]
-        public void WarningMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Warn("Warning message");
-            Assert.Equal("[WARN] Warning message", output.ToString().TrimEnd());
-        }
+		[Fact]
+		public void WarningMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Warn("Warning message");
+			Assert.Equal("[WARN] Warning message", output.ToString().TrimEnd());
+		}
 
-        [Fact]
-        public void InfoMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Info("Info message");
-            Assert.Equal("[INFO] Info message", output.ToString().TrimEnd());
-        }
+		[Fact]
+		public void InfoMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Info("Info message");
+			Assert.Equal("[INFO] Info message", output.ToString().TrimEnd());
+		}
 
-        [Fact]
-        public void DebugMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Debug("Debug message");
-            Assert.Equal("[DEBUG] Debug message", output.ToString().TrimEnd());
-        }
+		[Fact]
+		public void NoticeMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Notice("Info message");
+			Assert.Equal("[NOTICE] Info message", output.ToString().TrimEnd());
+		}
 
-        [Fact]
-        public void ProgressMessagesLogged() {
-            var output = new StringWriter();
-            Console.SetOut(output);
-            Logger.Progress("Progress message");
-            Assert.Equal("[PROGRESS] Progress message", output.ToString().TrimEnd());
-        }
-    }
+		[Fact]
+		public void DebugMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Debug("Debug message");
+			Assert.Equal("[DEBUG] Debug message", output.ToString().TrimEnd());
+		}
+
+		[Fact]
+		public void ProgressMessagesLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+			Logger.Progress("Progress message");
+			Assert.Equal("[PROGRESS] Progress message", output.ToString().TrimEnd());
+		}
+	}
 }
--- commonItems/LogExtensions.cs
+++ commonItems/LogExtensions.cs
@@ -2,24 +2,39 @@
 using log4net.Core;
 
 namespace commonItems {
-    public static class LogExtensions {
-        public static readonly Level progressLevel = new(35000, "PROGRESS");
+	public static class LogExtensions {
+		public static readonly Level progressLevel = new(35000, "PROGRESS");
 
-        public static void Progress(this ILog log, string message) {
-            var currentMethod = System.Reflection.MethodBase.GetCurrentMethod();
-            if (currentMethod is not null) {
-                log.Logger.Log(currentMethod.DeclaringType,
-                progressLevel, message, null);
-            }
-        }
+		public static void Progress(this ILog log, string message) {
+			var currentMethod = System.Reflection.MethodBase.GetCurrentMethod();
+			if (currentMethod is not null) {
+				log.Logger.Log(currentMethod.DeclaringType,
+				progressLevel, message, null);
+			}
+		}
+		public static void ProgressFormat(this ILog log, string message, params object[] args) {
+			string formattedMessage = string.Format(message, args);
+			var currentMethod = System.Reflection.MethodBase.GetCurrentMethod();
+			if (currentMethod is not null) {
+				log.Logger.Log(currentMethod.DeclaringType,
+				progressLevel, formattedMessage, null);
+			}
+		}
 
-        public static void ProgressFormat(this ILog log, string message, params object[] args) {
-            string formattedMessage = string.Format(message, args);
-            var currentMethod = System.Reflection.MethodBase.GetCurrentMethod();
-            if (currentMethod is not null) {
-                log.Logger.Log(currentMethod.DeclaringType,
-                progressLevel, formattedMessage, null);
-            }
-        }
-    }
+		public static void Notice(this ILog log, string message) {
+			var currentMethod = System.Reflection.MethodBase.GetCurrentMethod();
+			if (currentMethod is not null) {
+				log.Logger.Log(currentMethod.DeclaringType,
+				Level.Notice, message, null);
+			}
+		}
+		public static void NoticeFormat(this ILog log, string message, params object[] args) {
+			string formattedMessage = string.Format(message, args);
+			var currentMethod = System.Reflection.MethodBase.GetCurrentMethod();
+			if (currentMethod is not null) {
+				log.Logger.Log(currentMethod.DeclaringType,
+				Level.Notice, formattedMessage, null);
+			}
+		}
+	}
 }
