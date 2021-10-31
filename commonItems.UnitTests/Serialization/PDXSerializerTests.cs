31245e7d30ef9bfaf9f81509fc55d1b64abbf76a
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-10-31 13:25:04 +0100

    PDXSerializer: support for bool

--- commonItems.UnitTests/Serialization/PDXSerializerTests.cs
+++ commonItems.UnitTests/Serialization/PDXSerializerTests.cs
@@ -1,20 +1,25 @@
 ﻿using commonItems.Serialization;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace commonItems.UnitTests.Serialization {
 	public class PDXSerializerTests {
 		private class Title : IPDXSerializable {
 			public int id = 20;
+			public ulong capital_prov_id = 420;
+			public double development = 50.5;
 			[NonSerialized] public int priority = 50;
 			public string name = "Papal States";
-			public List<string> pope_names_list = new(){"Peter", "John", "Hadrian"};
-			public Color color1 = new (new[] {2, 4, 6});
+			public List<string> pope_names_list = new() { "Peter", "John", "Hadrian" };
+			public Color color1 = new(new[] { 2, 4, 6 });
 			public ParadoxBool landless = new(true);
+			public bool definite_form = false;
+
+			public Dictionary<string, string> textures = new() {
+				{ "diffuse", "gfx/models/diffuse.dds" },
+				{ "normal", "gfx/models/normal.dds" }
+			};
 		}
 
 		[Fact]
@@ -24,10 +29,17 @@ namespace commonItems.UnitTests.Serialization {
 
 			var expectedString = "{" + Environment.NewLine +
 								 "\tid = 20" + Environment.NewLine +
+								 "\tcapital_prov_id = 420" + Environment.NewLine +
+								 "\tdevelopment = 50.5" + Environment.NewLine +
 								 "\tname = \"Papal States\"" + Environment.NewLine +
 								 "\tpope_names_list = { \"Peter\", \"John\", \"Hadrian\" }" + Environment.NewLine +
 								 "\tcolor1 = { 2 4 6 }" + Environment.NewLine +
 								 "\tlandless = yes" + Environment.NewLine +
+								 "\tdefinite_form = no" + Environment.NewLine +
+								 "\ttextures = {" + Environment.NewLine +
+								 "\t\tdiffuse = \"gfx/models/diffuse.dds\"" + Environment.NewLine +
+								 "\t\tnormal = \"gfx/models/normal.dds\"" + Environment.NewLine +
+								 "\t}" + Environment.NewLine +
 								 "}" + Environment.NewLine;
 			Assert.Equal(expectedString, titleString);
 		}
--- commonItems/Serialization/PDXSerializer.cs
+++ commonItems/Serialization/PDXSerializer.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 
 namespace commonItems.Serialization {
 	public static class PDXSerializer {
-		public static string Serialize(IPDXSerializable obj) {
+		private static CultureInfo cultureInfo = CultureInfo.InvariantCulture;
+		public static string Serialize(IPDXSerializable obj, string indent = "") {
 			var sb = new StringBuilder();
 			var type = obj.GetType();
 			var fieldsInfo = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
 			Logger.Debug($"The fields of {type} are:");
-			sb.AppendLine("{");
+			sb.Append(indent).AppendLine("{");
 			foreach (var field in fieldsInfo) {
 				if (field.IsNotSerialized) {
 					Logger.Debug("Skipping NonSerialized " + field.Name); // TODO: REMOVE DEBUG LOGGING
@@ -24,39 +26,47 @@ namespace commonItems.Serialization {
 					continue;
 				}
 
-				sb.Append('\t').Append(field.Name).Append(" = ");
-
-				switch (fieldValue) {
-					case string str:
-						// add double quotes to string
-						sb.AppendLine(GetValueForSerializer(str));
-						break;
-					case Dictionary<Type, string> stringValueDict:
-						sb.AppendLine("{");
-						foreach (var (key, value) in stringValueDict) {
-							sb.Append(key).Append(" = ").AppendLine(value);
-						}
-						sb.AppendLine("}");
-						break;
-					case IEnumerable<string> strEnumerable:
-						IEnumerable<string> list = strEnumerable.ToList();
-						var strEnumerableWithDoubleQuotes = list.Select(GetValueForSerializer);
-						sb.AppendLine($"{{ {string.Join(", ", strEnumerableWithDoubleQuotes)} }}");
-						break;
-					case IPDXSerializable serializableType:
-						sb.AppendLine(serializableType.Serialize());
-						break;
-					default:
-						sb.AppendLine(fieldValue.ToString());
-						break;
+				var valueRepresentation = GetValueRepresentation(field, fieldValue, indent);
+				if (valueRepresentation is null) {
+					continue;
 				}
+				sb.Append(indent).Append('\t').Append(field.Name).Append(" = ").AppendLine(valueRepresentation);
 			}
-			sb.AppendLine("}");
+			sb.Append(indent).AppendLine("}");
 			return sb.ToString();
 		}
 
-		private static string GetValueForSerializer(string value) {
+		private static string GetStringRepresentation(string value) {
 			return $"\"{value}\"";
+		}
+
+		private static string? GetValueRepresentation(MemberInfo field, object fieldValue, string indent) {
+			var sb = new StringBuilder();
+			if (fieldValue is string str) {
+				sb.Append(indent).Append(GetStringRepresentation(str));
+			} else if (fieldValue is IDictionary<string, string> stringValueDict) {
+				sb.Append(indent).AppendLine("{");
+				foreach (var (key, value) in stringValueDict!) {
+					sb.Append(indent).Append("\t\t").Append(key).Append(" = ").AppendLine(GetStringRepresentation(value));
+				}
+				sb.Append(indent).Append("\t}");
+			} else if (fieldValue is IEnumerable<string> strEnumerable) {
+				IEnumerable<string> list = strEnumerable.ToList();
+				var strEnumerableWithDoubleQuotes = list.Select(GetStringRepresentation);
+				sb.Append(indent).Append("{ ").AppendJoin(", ", strEnumerableWithDoubleQuotes).Append(" }");
+			} else if (fieldValue is IPDXSerializable serializableType) {
+				sb.Append(indent).Append(serializableType.Serialize());
+			} else if (fieldValue is bool boolValue) {
+				var pdxBool = new ParadoxBool(boolValue);
+				sb.Append(pdxBool.YesOrNo);
+			} else if (fieldValue.GetType().IsValueType && fieldValue is IFormattable fp) { // for numbers
+				sb.Append(fp.ToString("G", cultureInfo));
+			} else {
+				Logger.Error($"Field {field.Name} of type {fieldValue.GetType()} is not yet supported by PDXSerializer!");
+				return null;
+			}
+
+			return sb.ToString();
 		}
 	}
 }
