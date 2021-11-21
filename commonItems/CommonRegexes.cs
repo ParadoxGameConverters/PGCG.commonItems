34719b5203c59d592539e036dd6bb898c5b11c2a
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-11-21 14:38:59 +0100

    Remove RegexOptions.Compiled

--- commonItems/CommonRegexes.cs
+++ commonItems/CommonRegexes.cs
@@ -5,24 +5,24 @@ namespace commonItems {
 		// catchall:
 		//		We grab everything that's NOT =, { or }, OR we grab everything within quotes, except newlines, which we already drop
 		//		in the parser.
-		public static Regex Catchall => new(@"^"".+""|[^={}]+$", RegexOptions.Compiled);
+		public static Regex Catchall => new(@"^"".+""|[^={}]+$");
 
 		// variables and interpolated expressions
-		public static Regex Variable => new($"^@[^{NonStringCharacters}]+$", RegexOptions.Compiled);
-		public static Regex InterpolatedExpression => new(@"@([\s\S]+)|(\[[\s\S]*\])$", RegexOptions.Compiled);
+		public static Regex Variable => new($"^@[^{NonStringCharacters}]+$");
+		public static Regex InterpolatedExpression => new(@"@([\s\S]+)|(\[[\s\S]*\])$");
 
 		// numbers
-		public static Regex Integer => new(@"^-?\d+$", RegexOptions.Compiled);
-		public static Regex QuotedInteger => new(@"^""-?\d+""$", RegexOptions.Compiled);
-		public static Regex Float => new(@"^-?\d+(.\d+)?$", RegexOptions.Compiled);
-		public static Regex QuotedFloat => new(@"^""-?\d+(.\d+)?""$", RegexOptions.Compiled);
+		public static Regex Integer => new(@"^-?\d+$");
+		public static Regex QuotedInteger => new(@"^""-?\d+""$");
+		public static Regex Float => new(@"^-?\d+(.\d+)?$");
+		public static Regex QuotedFloat => new(@"^""-?\d+(.\d+)?""$");
 
 		// strings
-		public static Regex String => new($"^(?!@).+[^{NonStringCharacters}]+$", RegexOptions.Compiled);
-		public static Regex QuotedString => new(@"^""[^\n=\{\}\""]+""$", RegexOptions.Compiled);
+		public static Regex String => new($"^(?!@).+[^{NonStringCharacters}]+$");
+		public static Regex QuotedString => new(@"^""[^\n=\{\}\""]+""$");
 
 		// dates
-		public static Regex Date => new(@"^\d+[.]\d+[.]\d+$", RegexOptions.Compiled);
+		public static Regex Date => new(@"^\d+[.]\d+[.]\d+$");
 
 		// characters that can't be part of an unquoted string
 		private const string NonStringCharacters = @"\s=\{\}\[\]\""";
