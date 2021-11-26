929d2e39860149b860f2c746b56b527cd5d15767
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-11-26 17:21:34 +0100

    IdObjectCollection

---
+++ commonItems.UnitTests/Collections/IdObjectCollectionTests.cs
@@ -0,0 +1,28 @@
+﻿using commonItems.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace commonItems.UnitTests.Collections {
+	public class IdObjectCollectionTests {
+		private class Character : IIdentifiable<string> {
+			public string Id { get; }
+
+			public Character(string id) {
+				Id = id;
+			}
+		}
+		private class Characters : IdObjectCollection<string,Character> { }
+
+		[Fact]
+		public void ObjectCanBeAdded() {
+			var characters = new Characters();
+			characters.Add(new Character("bob"));
+			characters.Add(new Character("frank"));
+			Assert.Collection();
+		}
+	}
+}
---
+++ commonItems/Collections/IdObjectCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace commonItems.Collections {
+	public class IdObjectCollection<TKey, TValue> : IReadOnlyDictionary<TKey, TValue> where TKey : notnull where TValue : IIdentifiable<TKey> {
+		private readonly Dictionary<TKey, TValue> dict = new();
+
+		public TValue this[TKey key] => dict[key];
+		public IEnumerable<TKey> Keys => dict.Keys;
+		public IEnumerable<TValue> Values => dict.Values;
+		public int Count => dict.Count;
+		public bool ContainsKey(TKey key) {
+			return dict.ContainsKey(key);
+		}
+		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
+			return dict.GetEnumerator();
+		}
+		public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value) {
+			return dict.TryGetValue(key, out value);
+		}
+		IEnumerator IEnumerable.GetEnumerator() {
+			return dict.GetEnumerator();
+		}
+
+		public virtual void Add(TValue obj) {
+			dict.Add(obj.Id, obj);
+		}
+
+		public virtual void Remove(TKey key) {
+			dict.Remove(key);
+		}
+	}
+}
