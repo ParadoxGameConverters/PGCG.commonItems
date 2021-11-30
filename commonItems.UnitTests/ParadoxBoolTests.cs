7ce89dd9759ff34aebdf0288504e17996ccf3b64
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-11-30 17:45:06 +0100

    Rename ParadoxBool to PDXBool

--- commonItems.UnitTests/ParadoxBoolTests.cs
+++ commonItems.UnitTests/ParadoxBoolTests.cs
@@ -5,40 +5,40 @@ namespace commonItems.UnitTests {
 	public class ParadoxBoolTests {
 		[Fact]
 		public void ParadoxBoolDefaultToTrue() {
-			Assert.True(new ParadoxBool());
+			Assert.True(new PDXBool());
 		}
 		[Fact]
 		public void ParadoxBoolCanBeConstructedFromBool() {
-			Assert.True(new ParadoxBool(true));
-			Assert.False(new ParadoxBool(false));
+			Assert.True(new PDXBool(true));
+			Assert.False(new PDXBool(false));
 		}
 		[Fact]
 		public void ParadoxBoolCanBeConstructedFromString() {
-			Assert.True(new ParadoxBool("yes"));
-			Assert.False(new ParadoxBool("no"));
+			Assert.True(new PDXBool("yes"));
+			Assert.False(new PDXBool("no"));
 		}
 		[Fact]
 		public void ExceptionIsThrownOnWrongValueString() {
 			const string valStr = "perhaps";
-			Assert.Throws<FormatException>(() => new ParadoxBool(valStr));
+			Assert.Throws<FormatException>(() => new PDXBool(valStr));
 		}
 		[Fact]
 		public void ParadoxBoolCanBeConstructedFromBufferedReader() {
 			var reader1 = new BufferedReader("= yes");
 			var reader2 = new BufferedReader("= no");
-			Assert.True(new ParadoxBool(reader1));
-			Assert.False(new ParadoxBool(reader2));
+			Assert.True(new PDXBool(reader1));
+			Assert.False(new PDXBool(reader2));
 		}
 		[Fact]
 		public void CorrectTextRepresentationIsReturned() {
-			var bool1 = new ParadoxBool("yes");
-			var bool2 = new ParadoxBool("no");
+			var bool1 = new PDXBool("yes");
+			var bool2 = new PDXBool("no");
 			Assert.Equal("yes", bool1.YesOrNo);
 			Assert.Equal("no", bool2.YesOrNo);
 		}
 		[Fact]
 		public void ValueCanBeChanged() {
-			var pdxBool = new ParadoxBool("no");
+			var pdxBool = new PDXBool("no");
 			Assert.False(pdxBool);
 			pdxBool.Value = true;
 			Assert.True(pdxBool);
