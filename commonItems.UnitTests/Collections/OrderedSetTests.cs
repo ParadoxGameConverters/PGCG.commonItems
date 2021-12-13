d1635a1129e84e9397cba9896007655d0d9eaf24
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-13 05:37:26 +0100

    OrderedSet collection (#84)
    * OrderedSet
    
    * OrderedSet
    
    * Satisfy Codacy

---
+++ commonItems.UnitTests/Collections/OrderedSetTests.cs
@@ -0,0 +1,54 @@
+﻿using commonItems.Collections;
+using Xunit;
+
+namespace commonItems.UnitTests.Collections {
+	public class OrderedSetTests {
+		[Fact]
+		public void InsertionOrderIsPreserved() {
+			var set = new OrderedSet<int> { 1, 5, 2, 4, 3 };
+
+			Assert.Collection(set,
+				item => Assert.Equal(1, item),
+				item => Assert.Equal(5, item),
+				item => Assert.Equal(2, item),
+				item => Assert.Equal(4, item),
+				item => Assert.Equal(3, item));
+		}
+
+		[Fact]
+		public void OrderedSetCanBeCleared() {
+			var set = new OrderedSet<int> { 1, 5, 2, 4, 3 };
+
+			set.Clear();
+			Assert.Empty(set);
+		}
+
+		[Fact]
+		public void RemovingAndReaddingElementChangesInsertionOrder() {
+			var set = new OrderedSet<int> { 1, 5, 2, 4, 3 };
+
+			set.Remove(5);
+			Assert.DoesNotContain(5, set);
+			Assert.Collection(set,
+				item => Assert.Equal(1, item),
+				item => Assert.Equal(2, item),
+				item => Assert.Equal(4, item),
+				item => Assert.Equal(3, item));
+
+			set.Add(5);
+			Assert.Contains(5, set);
+			Assert.Collection(set,
+				item => Assert.Equal(1, item),
+				item => Assert.Equal(2, item),
+				item => Assert.Equal(4, item),
+				item => Assert.Equal(3, item),
+				item => Assert.Equal(5, item));
+		}
+
+		[Fact]
+		public void RemoveReturnsFalseOnElementNotFound() {
+			var set = new OrderedSet<int>();
+			Assert.False(set.Remove(3));
+		}
+	}
+}
---
+++ commonItems/Collections/OrderedSet.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace commonItems.Collections;
+
+// A set that preserves insertion order
+// https://stackoverflow.com/a/17853085/10249243
+public class OrderedSet<T> : ICollection<T> where T : notnull {
+	private readonly IDictionary<T, LinkedListNode<T>> dictionary;
+	private readonly LinkedList<T> linkedList;
+
+	public OrderedSet() : this(EqualityComparer<T>.Default) { }
+
+	public OrderedSet(IEqualityComparer<T> comparer) {
+		dictionary = new Dictionary<T, LinkedListNode<T>>(comparer);
+		linkedList = new();
+	}
+
+	public int Count => dictionary.Count;
+
+	public virtual bool IsReadOnly => dictionary.IsReadOnly;
+
+	void ICollection<T>.Add(T item) => Add(item);
+
+	public bool Add(T item) {
+		if (dictionary.ContainsKey(item)) {
+			return false;
+		}
+		var node = linkedList.AddLast(item);
+		dictionary.Add(item, node);
+		return true;
+	}
+
+	public void Clear() {
+		linkedList.Clear();
+		dictionary.Clear();
+	}
+
+	public bool Remove(T item) {
+		var found = dictionary.TryGetValue(item, out var node);
+		if (!found) {
+			return false;
+		}
+		dictionary.Remove(item);
+		linkedList.Remove(node!);
+		return true;
+	}
+
+	public IEnumerator<T> GetEnumerator() => linkedList.GetEnumerator();
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	public bool Contains(T item) => dictionary.ContainsKey(item);
+
+	public void CopyTo(T[] array, int arrayIndex) => linkedList.CopyTo(array, arrayIndex);
+}
