c51a1bff5d844b19dcf726c2d750d1cca3ad3718
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-05-24 07:56:59 +0200

    StringOfItem constructor from string (#142)
    * StringOfItem constructor from string
    
    * Remove BlobList's inheritance from Parser
    
    * StringOfItemCanBeConstructedFromString test

---
+++ commonItems.UnitTests/StringOfItemTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace commonItems.UnitTests; 
+
+public class StringOfItemTests {
+	[Fact]
+	public void StringOfItemConvertsBracedObjectsToStrings() {
+		const string input =
+			@"= {\n
+                \t{\n
+                \t\tid = 180\n
+                \t\ttype = 46\n
+                \t}\n
+                }";
+		var reader = new BufferedReader(input);
+
+		var theItem = reader.GetStringOfItem();
+		Assert.Equal(input[2..], theItem.ToString());
+	}
+	
+	[Fact]
+	public void StringOfItemGetsStringAfterEquals() {
+		var reader = new BufferedReader(" = foo");
+		var theItem = reader.GetStringOfItem();
+		Assert.Equal("foo", theItem.ToString());
+	}
+
+	[Fact]
+	public void StringOfItemCanBeConstructedFromString() {
+		const string str = "{ key=value }";
+		Assert.Equal(str, new StringOfItem(str).ToString());
+	}
+}
---
+++ commonItems/StringOfItem.cs
@@ -0,0 +1,42 @@
+﻿using commonItems.Serialization;
+using System.Text;
+
+namespace commonItems;
+
+public class StringOfItem : IPDXSerializable {
+	public StringOfItem(string itemString) {
+		str = itemString;
+	}
+	public StringOfItem(BufferedReader reader) {
+		var next = Parser.GetNextLexeme(reader);
+		var sb = new StringBuilder();
+		if (next == "=") {
+			next = Parser.GetNextLexeme(reader);
+		}
+		sb.Append(next);
+
+		if (next == "{") {
+			var braceDepth = 1;
+			while (!reader.EndOfStream) {
+				char inputChar = (char)reader.Read();
+				sb.Append(inputChar);
+
+				if (inputChar == '{') {
+					++braceDepth;
+				} else if (inputChar == '}') {
+					--braceDepth;
+					if (braceDepth == 0) {
+						str = sb.ToString();
+						return;
+					}
+				}
+			}
+		}
+		str = sb.ToString();
+	}
+	
+	public string Serialize(string indent, bool withBraces) => ToString();
+	public override string ToString() => str;
+
+	private readonly string str;
+}
