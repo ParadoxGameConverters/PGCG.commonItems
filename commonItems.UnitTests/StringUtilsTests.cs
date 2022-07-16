9112077a266c9a115aec20d1e6132f4d90e471e4
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-07-16 10:46:03 +0200

    Make RemQuotes and AddQuotes extension methods

--- commonItems.UnitTests/StringUtilsTests.cs
+++ commonItems.UnitTests/StringUtilsTests.cs
@@ -8,6 +8,7 @@ public class StringUtilsTests {
 	[Fact]
 	public void RemQuotesRemovesQuotes() {
 		const string quotedString = "\"Quoted string\"";
+		// ReSharper disable once InvokeAsExtensionMethod
 		string unquotedString = StringUtils.RemQuotes(quotedString);
 		Assert.Equal("Quoted string", unquotedString);
 	}
@@ -15,27 +16,28 @@ public class StringUtilsTests {
 	[Fact]
 	public void RemQuotesRequiresStartingQuotes() {
 		const string quotedString = "Quoted string\"";
-		string unquotedString = StringUtils.RemQuotes(quotedString);
+		string unquotedString = quotedString.RemQuotes();
 		Assert.Equal("Quoted string\"", unquotedString);
 	}
 
 	[Fact]
 	public void RemQuotesRequiresEndingQuotes() {
 		const string quotedString = "\"Quoted string";
-		string unquotedString = StringUtils.RemQuotes(quotedString);
+		string unquotedString = quotedString.RemQuotes();
 		Assert.Equal("\"Quoted string", unquotedString);
 	}
 
 	[Fact]
 	public void RemQuotesLeavesSingleQuote() {
 		const string quotedString = "\"";
-		string unquotedString = StringUtils.RemQuotes(quotedString);
+		string unquotedString = quotedString.RemQuotes();
 		Assert.Equal("\"", unquotedString);
 	}
 
 	[Fact]
 	public void AddQuotesTurnsEmptyStringIntoQuoted() {
 		const string unquotedString = "";
+		// ReSharper disable once InvokeAsExtensionMethod
 		string quotedString = StringUtils.AddQuotes(unquotedString);
 		Assert.Equal("\"\"", quotedString);
 	}
@@ -43,28 +45,28 @@ public class StringUtilsTests {
 	[Fact]
 	public void AddQuotesLeavesAlreadyQuotedStringAlone() {
 		const string alreadyQuotedString = "\"already quoted\"";
-		string quotedString = StringUtils.AddQuotes(alreadyQuotedString);
+		string quotedString = alreadyQuotedString.AddQuotes();
 		Assert.Equal("\"already quoted\"", quotedString);
 	}
 
 	[Fact]
 	public void AddQuotesAddsQuotesToUnquotedString() {
 		const string unqQuotedString = "not quoted";
-		string quotedString = StringUtils.AddQuotes(unqQuotedString);
+		string quotedString = unqQuotedString.AddQuotes();
 		Assert.Equal("\"not quoted\"", quotedString);
 	}
 
 	[Fact]
 	public void AddQuotesLeavesSingleQuote() {
 		const string singleQuoteString = "\"";
-		string quotedString = StringUtils.AddQuotes(singleQuoteString);
+		string quotedString = singleQuoteString.AddQuotes();
 		Assert.Equal("\"", quotedString);
 	}
 
 	[Fact]
 	public void AddQuotesLeavesImproperlyQuotedStringAsIs() {
 		const string improperlyQuotedString = "\"c";
-		string quotedString = StringUtils.AddQuotes(improperlyQuotedString);
+		string quotedString = improperlyQuotedString.AddQuotes();
 		Assert.Equal("\"c", quotedString);
 	}
 }
