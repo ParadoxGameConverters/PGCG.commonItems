729f10a5710b419606a1c7b5ca5349f347578d55
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-07-24 18:45:12 +0200

    IgnoreAndStoreUnregisteredItems extension method (#164) #minor

---
+++ commonItems.UnitTests/ParserExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace commonItems.UnitTests; 
+
+public class ParserExtensionsTests {
+	[Fact]
+	public void UnregisteredItemsCanBeStoredInASet() {
+		var reader = new BufferedReader(@"
+			a={}
+			1 = {}
+			ignore_me_1={}
+			ignore_me_2 = {}");
+		var ignoredTokens = new HashSet<string>();
+		
+		Assert.Empty(ignoredTokens);
+		
+		var parser = new Parser();
+		parser.RegisterKeyword("a", ParserHelpers.IgnoreItem);
+		parser.RegisterRegex(CommonRegexes.Integer, ParserHelpers.IgnoreItem);
+		parser.IgnoreAndStoreUnregisteredItems(ignoredTokens);
+		
+		parser.ParseStream(reader);
+		ignoredTokens.Should().BeEquivalentTo("ignore_me_1", "ignore_me_2");
+	}
+}
