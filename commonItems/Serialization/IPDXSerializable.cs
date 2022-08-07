6c8be5b0386e6f459a1e38e9980b51c0de9b8d24
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-08-07 13:35:08 +0200

    Extract IPDXSerializable.SerializeMembers from Serialize

--- commonItems/Serialization/IPDXSerializable.cs
+++ commonItems/Serialization/IPDXSerializable.cs
@@ -5,14 +5,10 @@ using System.Text;
 namespace commonItems.Serialization; 
 
 public interface IPDXSerializable {
-	public string Serialize(string indent, bool withBraces) {
-		// default implementation: serialize members
-		var sb = new StringBuilder();
+	public string SerializeMembers(string indent) {
 		var type = GetType();
 		var mi = type.GetMembers(BindingFlags.Public | BindingFlags.Instance);
-		if (withBraces) {
-			sb.AppendLine("{");
-		}
+		var sb = new StringBuilder();
 		foreach (var member in mi) {
 			if (member is not PropertyInfo && member is not FieldInfo) {
 				continue;
@@ -24,21 +20,27 @@ public interface IPDXSerializable {
 			if (fieldValue is null) {
 				continue;
 			}
-
-			var internalIndent = "";
-			if (withBraces) {
-				internalIndent += '\t';
-			}
+			
 			string lineRepresentation;
 			if (Attribute.IsDefined(member, typeof(SerializeOnlyValue))) {
-				lineRepresentation = PDXSerializer.Serialize(fieldValue, indent + internalIndent, false);
+				lineRepresentation = PDXSerializer.Serialize(fieldValue, indent, false);
 			} else {
-				lineRepresentation = member.GetName() + '=' + PDXSerializer.Serialize(fieldValue, indent + '\t');
+				lineRepresentation = $"{member.GetName()}={PDXSerializer.Serialize(fieldValue, indent)}";
 			}
 			if (!string.IsNullOrWhiteSpace(lineRepresentation)) {
-				sb.Append(indent).Append(internalIndent).AppendLine(lineRepresentation);
+				sb.Append(indent).AppendLine(lineRepresentation);
 			}
 		}
+
+		return sb.ToString();
+	}
+	public string Serialize(string indent, bool withBraces) {
+		// default implementation: serialize members
+		var sb = new StringBuilder();
+		if (withBraces) {
+			sb.AppendLine("{");
+		}
+		sb.Append(SerializeMembers(withBraces ? indent + '\t' : indent));
 		if (withBraces) {
 			sb.Append(indent).Append('}');
 		}
