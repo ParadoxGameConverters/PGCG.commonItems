03812c3fb95a1ec0f8d1d12fbbb81f66e007abe5
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-08-17 23:11:29 +0200

    Expand vowels string

--- commonItems/Linguistics/CharacterExtensions.cs
+++ commonItems/Linguistics/CharacterExtensions.cs
@@ -3,7 +3,7 @@
 namespace commonItems.Linguistics; 
 
 public static class CharacterExtensions {
-	private const string Vowels = "aeiou";
+	private const string Vowels = "AÀÁÂÃÄÅĀĂĄǺȀȂẠẢẤẦẨẪẬẮẰẲẴẶḀÆǼEȄȆḔḖḘḚḜẸẺẼẾỀỂỄỆĒĔĖĘĚÈÉÊËIȈȊḬḮỈỊĨĪĬĮİÌÍÎÏĲOŒØǾȌȎṌṎṐṒỌỎỐỒỔỖỘỚỜỞỠỢŌÒÓŎŐÔÕÖUŨŪŬŮŰŲÙÚÛÜȔȖṲṴṶṸṺỤỦỨỪỬỮỰYẙỲỴỶỸŶŸÝ";
 	
 	public static bool IsVowel(this char character) {
 		bool isVowel = Vowels.Contains(character, StringComparison.InvariantCultureIgnoreCase);
