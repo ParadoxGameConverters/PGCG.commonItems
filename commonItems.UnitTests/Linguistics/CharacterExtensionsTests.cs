63ecf81f9b86c88f8e974156b3d2d9b21b324b79
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-08-17 19:35:22 +0200

    IsVowelReturnsCorrectValue test

---
+++ commonItems.UnitTests/Linguistics/CharacterExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using commonItems.Linguistics;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace commonItems.UnitTests.Linguistics; 
+
+[SuppressMessage("ReSharper", "StringLiteralTypo")]
+public class CharacterExtensionsTests {
+	[Theory]
+	[InlineData("aeiou", true)]
+	[InlineData("qwrtxcvb", false)]
+	public void IsVowelReturnsCorrectValue(IEnumerable<char> testCharacter, bool expectedValue) {
+		foreach (var c in testCharacter) {
+			Assert.Equal(expectedValue, c.IsVowel());
+		}
+	}
+}
