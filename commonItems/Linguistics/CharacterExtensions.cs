eb45bdadb806353351b4a5e84f9ef6418ab4362c
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-08-17 19:30:43 +0200

    IsVowel extension method

---
+++ commonItems/Linguistics/CharacterExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace commonItems.Linguistics; 
+
+public static class CharacterExtensions {
+	private const string Vowels = "aeiou";
+	
+	public static bool IsVowel(this char character) {
+		bool isVowel = Vowels.Contains(character, StringComparison.InvariantCultureIgnoreCase);
+		return isVowel;
+	}
+}
