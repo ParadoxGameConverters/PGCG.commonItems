5b0ca56143d09dd34f81b243ed41508473623e78
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-08-17 23:23:02 +0200

    Linguistics/StringExtensions.cs

---
+++ commonItems/Linguistics/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace commonItems.Linguistics; 
+
+public static class StringExtensions {
+	public static string GetAdjective(this string str) {
+		return str + "ish"; // TODO: replace placeholder
+	}
+}
