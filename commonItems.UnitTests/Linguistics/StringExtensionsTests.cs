2d138c13af02cb8d57faa58091422fb703d1d20a
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-09-16 17:45:32 +0200

    Add CSV file with adjectives test data

--- commonItems.UnitTests/Linguistics/StringExtensionsTests.cs
+++ commonItems.UnitTests/Linguistics/StringExtensionsTests.cs
@@ -1,4 +1,7 @@
 ﻿using commonItems.Linguistics;
+using Csv;
+using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace commonItems.UnitTests.Linguistics;
@@ -366,6 +369,27 @@ public class StringExtensionsTests {
 	// ReSharper restore StringLiteralTypo
 	public void GetAdjectiveGeneratesCorrectishAdjective(string noun, string expectedAdjective) {
 		Assert.Equal(expectedAdjective, noun.GetAdjective());
+	}
+
+	[Fact]
+	public void CorrectAdjectivesAreGeneratedForCities() {
+		var filePath = "TestFiles/adjectives/cities.csv";
+		var csv = File.ReadAllText(filePath);
+		foreach (var line in CsvReader.ReadFromText(csv)) {
+			// Header is handled, each line will contain the actual row data.
+			var name = line[0];
+			var adj1 = line[1];
+			var adj2 = line[2];
+			var adj3 = line[3];
+			var adj4 = line[4];
+			var adj5 = line[5];
+
+			var validAdjectives = new[] {adj1, adj2, adj3, adj4, adj5}
+				.Where(adj=>!string.IsNullOrEmpty(adj));
+
+			var generatedAdj = name.GetAdjective();
+			Assert.Contains(generatedAdj, validAdjectives);
+		}
 	}
 
 	[Theory]
