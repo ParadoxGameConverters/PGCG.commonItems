82f2490bc8ac82385cfdbc19227de50c92b1eadd
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-10-30 01:33:15 +0200

    Add logging of info that is helpful for debugging

---
+++ commonItems/DebugInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace commonItems; 
+
+public static class DebugInfo {
+	public static void LogSystemInfo() {
+		OperatingSystem os = Environment.OSVersion;
+		Logger.DebugFormat("Operating system: {0}", os.VersionString);
+		CultureInfo ci = CultureInfo.InstalledUICulture;
+		Logger.DebugFormat("Installed UI language: {0}", ci.Name);
+	}
+
+	public static void LogExecutableLocation() {
+		var location = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+		Logger.Debug($"Executable location: {location}");
+	}
+
+	public static void LogEverything() {
+		LogSystemInfo();
+		LogExecutableLocation();
+	}
+}
