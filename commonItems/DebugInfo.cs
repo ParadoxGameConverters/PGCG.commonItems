00250f14983ba69b33cbc4d9b160a47688d0678e
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-10-30 20:23:45 +0100

    Log found antiviruses

--- commonItems/DebugInfo.cs
+++ commonItems/DebugInfo.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
 
 namespace commonItems; 
 
@@ -15,8 +18,35 @@ public static class DebugInfo {
 		Logger.Debug($"Executable directory: {AppDomain.CurrentDomain.BaseDirectory}");
 	}
 
+	public static void LogAntivirusInfo() {
+		if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+			return;
+		}
+		
+		var searcherPreVista = new ManagementObjectSearcher($@"\\{Environment.MachineName}\root\SecurityCenter", "SELECT * FROM AntivirusProduct");
+		var searcherPostVista = new ManagementObjectSearcher($@"\\{Environment.MachineName}\root\SecurityCenter2", "SELECT * FROM AntivirusProduct");
+		var preVistaResult = searcherPreVista.Get().OfType<ManagementObject>();
+		var postVistaResult = searcherPostVista.Get().OfType<ManagementObject>();
+
+		var instances = preVistaResult.Concat(postVistaResult);
+
+		var installedAntiviruses = instances
+			.Select(i => i.Properties.OfType<PropertyData>())
+			.Where(pd => pd.Any(p => p.Name == "displayName"))
+			.Select(pd => new {
+				Name = pd.Single(p => p.Name == "displayName").Value
+			})
+			.ToArray();
+
+		foreach (var antivirus in installedAntiviruses) {
+			Logger.Debug($"Found antivirus: {antivirus.Name}");
+		}
+
+	}
+
 	public static void LogEverything() {
 		LogSystemInfo();
 		LogExecutableDirectory();
+		LogAntivirusInfo();
 	}
 }
