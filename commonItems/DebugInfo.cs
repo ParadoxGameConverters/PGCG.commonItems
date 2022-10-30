743eb50550628b8deed8da206f304670b7d4a877
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-10-30 20:49:41 +0100

    Log found antiviruses (#235) #minor
    * Log found antiviruses
    
    * Add summary to function

--- commonItems/DebugInfo.cs
+++ commonItems/DebugInfo.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
 
 namespace commonItems; 
 
@@ -15,8 +18,37 @@ public static class DebugInfo {
 		Logger.Debug($"Executable directory: {AppDomain.CurrentDomain.BaseDirectory}");
 	}
 
+	/// <summary>
+	/// Based on https://stackoverflow.com/a/27168374/10249243.
+	/// </summary>
+	public static void LogAntivirusInfo() {
+		if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+			return;
+		}
+		
+		var searcherPreVista = new ManagementObjectSearcher($@"\\{Environment.MachineName}\root\SecurityCenter", "SELECT * FROM AntivirusProduct");
+		var searcherPostVista = new ManagementObjectSearcher($@"\\{Environment.MachineName}\root\SecurityCenter2", "SELECT * FROM AntivirusProduct");
+		var preVistaResult = searcherPreVista.Get().OfType<ManagementObject>();
+		var postVistaResult = searcherPostVista.Get().OfType<ManagementObject>();
+
+		var instances = preVistaResult.Concat(postVistaResult);
+
+#pragma warning disable CA1416
+		var installedAntiviruses = instances
+			.Select(i => i.Properties.OfType<PropertyData>())
+			.Where(pd => pd.Any(p => p.Name == "displayName"))
+			.Select(pd => pd.Single(p => p.Name == "displayName").Value.ToString())
+			.ToArray();
+#pragma warning restore CA1416
+
+		foreach (var antivirusName in installedAntiviruses) {
+			Logger.Debug($"Found antivirus: {antivirusName}");
+		}
+	}
+
 	public static void LogEverything() {
 		LogSystemInfo();
 		LogExecutableDirectory();
+		LogAntivirusInfo();
 	}
 }
