e148112729f1e67dbb66db68469c776847f52b27
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-05-10 19:33:43 +0200

    Expand GetSteamInstallPathTests

--- commonItems.UnitTests/GetSteamInstallPathTests.cs
+++ commonItems.UnitTests/GetSteamInstallPathTests.cs
@@ -8,14 +8,22 @@ namespace commonItems.UnitTests;
 [CollectionDefinition("Sequential", DisableParallelization = true)]
 public class GetSteamInstallPathTests {
     [Fact]
-    public void MessageIsLoggedAndNullIsReturnedWhenGameIsNotFound() {
+    public void NullIsReturnedWhenGameIsNotFound() {
         const int fakeSteamId = 420;
-
-		var output = new StringWriter();
-		Console.SetOut(output);
-
         var gamePath = CommonFunctions.GetSteamInstallPath(fakeSteamId);
+        
         Assert.Null(gamePath);
-        Assert.Contains($"Error occurred when locating Steam game {fakeSteamId}: ", output.ToString());
+    }
+
+    [Fact]
+    public void ErrorIsLoggedWhenSteamGameIdIsInvalid() {
+	    var output = new StringWriter();
+	    Console.SetOut(output);
+	    
+	    const int fakeSteamId = -1;
+	    var gamePath = CommonFunctions.GetSteamInstallPath(fakeSteamId);
+	    Assert.Null(gamePath);
+	    
+	    Assert.Contains($"Error occurred when locating Steam game {fakeSteamId}: ", output.ToString());
     }
 }
