49d6076dc55e7a21085c95e9b45b83ad0edd164f
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-05-10 19:58:04 +0200

    GetGOGInstallPathTests (#306)
    * Expand GetSteamInstallPathTests
    
    * GetGOGInstallPathTests

---
+++ commonItems.UnitTests/GetGOGInstallPathTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace commonItems.UnitTests; 
+
+public class GetGOGInstallPathTests {
+	[Fact]
+	public void NullIsReturnedWhenGameIsNotFound() {
+		const int fakeGOGId = 420;
+		var gamePath = CommonFunctions.GetGOGInstallPath(fakeGOGId);
+        
+		Assert.Null(gamePath);
+	}
+}
--- commonItems.UnitTests/GetSteamInstallPathTests.cs
+++ commonItems.UnitTests/GetSteamInstallPathTests.cs
@@ -1,21 +1,13 @@
-using System;
-using System.IO;
 using Xunit;
 
 namespace commonItems.UnitTests;
 
-[Collection("Sequential")]
-[CollectionDefinition("Sequential", DisableParallelization = true)]
 public class GetSteamInstallPathTests {
     [Fact]
-    public void MessageIsLoggedAndNullIsReturnedWhenGameIsNotFound() {
+    public void NullIsReturnedWhenGameIsNotFound() {
         const int fakeSteamId = 420;
-
-		var output = new StringWriter();
-		Console.SetOut(output);
-
         var gamePath = CommonFunctions.GetSteamInstallPath(fakeSteamId);
+        
         Assert.Null(gamePath);
-        Assert.Contains($"Error occurred when locating Steam game {fakeSteamId}: ", output.ToString());
     }
 }
