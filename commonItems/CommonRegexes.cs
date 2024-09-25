bc5139f59d0a601754eedc563bfe83709eb9b3c6
iht <IhateTrains@users.noreply.github.com>
2024-09-25 14:46:32 +0200

    Use GeneratedRegex for String and Variable regexes

--- commonItems/CommonRegexes.cs
+++ commonItems/CommonRegexes.cs
@@ -9,7 +9,7 @@ public static partial class CommonRegexes {
 	public static Regex Catchall => GetCatchallRegex();
 
 	// variables and interpolated expressions
-	public static Regex Variable => new($"^@[^{NonStringCharacters}]+$");
+	public static Regex Variable => GetVariableRegex();
 	public static Regex InterpolatedExpression => GetInterpolatedExpressionRegex();
 
 	// numbers
@@ -19,14 +19,13 @@ public static partial class CommonRegexes {
 	public static Regex QuotedFloat => GetQuotedFloatRegex();
 
 	// strings
-	public static Regex String => new($"^(?!@).*[^{NonStringCharacters}]+$");
+	// Characters that can't be part of an unquoted string: \s = \{\} \[ \] \"
+	public static Regex String => GetStringRegex();
 	public static Regex QuotedString => GetQuotedStringRegex();
 
 	// dates
 	public static Regex Date => GetDateRegex();
 
-	// characters that can't be part of an unquoted string
-	private const string NonStringCharacters = @"\s=\{\}\[\]\""";
 
 	[GeneratedRegex("^\".+\"|[^={}]+$")]
 	private static partial Regex GetCatchallRegex();
@@ -40,8 +39,16 @@ public static partial class CommonRegexes {
 	private static partial Regex GetQuotedFloatRegex();
 	[GeneratedRegex(@"^-?\d+([.]\d+)?([.]\d+)?\.?$")]
 	private static partial Regex GetDateRegex();
+	
+	[GeneratedRegex("^(?!@).*[^\\s=\\{\\}\\[\\]\\\"]+$")]
+	private static partial Regex GetStringRegex();
+	
 	[GeneratedRegex(@"^""[^\n\""]+""$")]
 	private static partial Regex GetQuotedStringRegex();
+	
+	[GeneratedRegex("^@[^\\s=\\{\\}\\[\\]\\\"]+$")]
+	private static partial Regex GetVariableRegex();
+	
 	[GeneratedRegex(@"^@([\s\S]+)|(\[[\s\S]*\])$")]
 	private static partial Regex GetInterpolatedExpressionRegex();
 }
